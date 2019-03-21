a495c54d0e4fe572a1db0c4914d8bea7321921a0
Sven Efftinge <sven.efftinge@typefox.io>
2019-03-21 08:35:00 +0000

    improved gitpod setup

--- dash_intro.py
+++ dash_intro.py
@@ -30,4 +30,4 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host="0.0.0.0")
--- run.sh
+++ run.sh
@@ -4,6 +4,3 @@ pip install dash-html-components==0.14.0  # HTML components
 pip install dash-core-components==0.44.0  # Supercharged components
 pip install dash-table==3.6.0  # Interactive DataTable component (new!)
 pip install dash-daq==0.1.0  # DAQ components (newly open-sourced!)
-
-# expose
-gp forward-port 8050 3000
