3798f5abe9638a899889c85a907052704e434f0e
Taiki Komoda <komoda@jins.com>
2019-03-22 03:20:37 +0000

    install python libraries in non-volatile directory

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # install dependencies
-pip install dash==0.39.0  # The core dash backend
-pip install dash-html-components==0.14.0  # HTML components
-pip install dash-core-components==0.44.0  # Supercharged components
-pip install dash-table==3.6.0  # Interactive DataTable component (new!)
-pip install dash-daq==0.1.0  # DAQ components (newly open-sourced!)
+pip install dash==0.39.0 -t site_packages # The core dash backend
+pip install dash-html-components==0.14.0 -t site_packages  # HTML components
+pip install dash-core-components==0.44.0 -t site_packages  # Supercharged components
+pip install dash-table==3.6.0 -t site_packages  # Interactive DataTable component (new!)
+pip install dash-daq==0.1.0 -t site_packages  # DAQ components (newly open-sourced!)
