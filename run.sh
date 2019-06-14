362a57457a589566b40b3c5134d2f7cffcaeb822
Taiki Komoda <komoda@jins.com>
2019-06-14 01:47:46 +0000

    fix

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # install dependencies
-pip3 install dash==0.43.0 # The core dash backend
-pip3 install dash-daq==0.1.0  # DAQ components (newly open-sourced!)
-pip3 jupyter
+pip3 install dash # ==0.43.0 The core dash backend
+pip3 install dash-daq # ==0.1.0 DAQ components (newly open-sourced!)
+pip3 install jupyter # install jupyter notebook
